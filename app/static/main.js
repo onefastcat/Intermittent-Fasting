ed33c9b117b4b5833d8836a3c339e37a7af437cc
onefastcat <a.kho.meow@gmail.com>
2023-01-11 13:46:29 -0500

    responsive tag and fast hours

--- app/static/main.js
+++ app/static/main.js
@@ -76,6 +76,11 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     }
 
+    //document.getElementsByClassName('hour').appendChild(div);
+   // const element = document.getElementsByClassName('container')[0];
+    ///element.appendChild(div);
+    //console.log(document.getElementsByClassName('container'));
+    //console.log(element);
 
 
     const x = window.matchMedia("(min-width: 700px)");
@@ -135,7 +140,7 @@ function mealTimeColor(mealsArr, fast){
 
         return;
     }
-
+    console.log("----------------meal  Color---------------------");
     document.addEventListener('DOMContentLoaded', () => {
 
      for (let meal of meals){
@@ -164,6 +169,8 @@ function fastWindowColor(fast, meals){
 
     const fastSchedule = JSON.parse(fast);
     const mealsList = JSON.parse(meals);
+    console.log("----------------fast  Window  Color---------------------");
+    console.log(fastSchedule)
 
 
     //if Fast schedule is not an array, it is a originalFastWindow
@@ -175,20 +182,23 @@ function fastWindowColor(fast, meals){
         let sundayFastHoursStart = startTime;
         let mondayFastHoursEnd = endTime;
 
-
+        const queryString = window.location.search;
+        const params = new URLSearchParams(queryString);
+        const week = params.get('week');
 
        //check if we are on /next-week url
-        if (window.location.href.indexOf("next-week") > -1) {
+        if (week === 'next') {
             let latestMeal = findLatestMeal(mealsList);
 
             //change next week fast time only if the Sunday meal is late enough
             if(latestMeal['timeOfMeal'] > startTime){
+                console.log('recalc fast monday')
                 extraFastHours = latestMeal['timeOfMeal'] - startTime + 1;
                 mondayFastHoursEnd = endTime + extraFastHours;
             }
         }
         //
-        else if (window.location.href.indexOf("previous-week") > -1) {
+        else if (week === 'previous') {
             let earliestMeal = findEarliestMeal(mealsList);
 
 
@@ -200,7 +210,7 @@ function fastWindowColor(fast, meals){
         }
 
         document.addEventListener('DOMContentLoaded', function() {
-
+           // console.log(fastSchedule)
             for(let i = 0; i < 7; i++) {
                 let day = determineDayFromIndex(i);
                 fastHour = document.getElementsByClassName(day)
@@ -233,13 +243,13 @@ function fastWindowColor(fast, meals){
     //maybe this event listener should listen to form being submitted
     //insted of domcontentLoaded
     else{
-
+        console.log('hereeeee')
         document.addEventListener('DOMContentLoaded', function() {
             for(let i = 0; i < 7; i++) {
              let day = determineDayFromIndex(i);
              fastHour = document.getElementsByClassName(day)
              //won't need to check if it is array because this loop is reused specifically for this. refactor later
-
+                console.log(day)
                 startTime = fastSchedule[i]['startFast'];
                 endTime = fastSchedule[i]['endFast']
 
@@ -273,7 +283,7 @@ function fastWindowColor(fast, meals){
 }
 
 function hasCertainDayMeal(meals, day){
-
+    console.log('what is goin on')
 
     for(let meal of meals){
 
@@ -292,7 +302,8 @@ function togglePrevNext(week){
     const corner = document.getElementsByClassName('corner')[0]
     lastWeek = corner.lastChild;
     nextWeek = corner.firstChild;
-
+    console.log('togggggggling')
+    console.log(nextWeek)
     if(week == 'next'){
         nextWeek.classList.remove('disabled');
     }
